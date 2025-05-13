318deb289e040a35b500d7e1ce05bd504f72ac7a
almothanna <148761376+almothanna@users.noreply.github.com>
2025-05-13 21:01:16 +0200

    draw

--- sketch.js
+++ sketch.js
@@ -11,8 +11,18 @@ function setup() {
   let player;
 
 function setup() {
-    
+
   createCanvas(600, 600);
   player = new Player();
 
 }
+
+function draw() {
+    background(220);
+    player.update();
+    player.show();
+  }
+  
+  function keyPressed() {
+    player.move(keyCode);
+  }
