7ba4f73a29b95d3e80c6084754e7e10fc3ec140b
almothanna <148761376+almothanna@users.noreply.github.com>
2025-05-16 17:30:38 +0200

    player moves easily all direction

--- sketch.js
+++ sketch.js
@@ -1,71 +1,53 @@
-
-function setup() {
-    createCanvas(600, 600);
-  }
-  
-  function draw() {
-    background(220);
-  }
-  
-
-  let player;
+let player;
 
 function setup() {
 
   createCanvas(600, 600);
   player = new Player();
-
 }
 
 function draw() {
-    background(220);
-    player.update();
-    player.show();
-  }
-  
-  function keyPressed() {
-    player.move(keyCode);
-  }
 
+  background(220);
+  player.update();
+  player.show();
 
-  function keyPressed() {
-
-    player.move(keyCode);
-
-  }
-  
-  class Player {
-    constructor() {
-
-      this.x = width / 2;
-      this.y = height - 22;
-      this.size = 22;
-    }
 }
 
-
-update()
-
+function keyPressed()
  {
-  this.x = constrain (this.x, 0 , width - this.size);
-  this.y = constrain (this.y, 0 , height - this.size);
+  player.move(keyCode);
 }
 
+class Player {
 
+  constructor() {
+    this.x = width / 2;
+    this.y = height - 22;
+    this.size = 22;
+  }
 
-show() 
-{
-  fill(0, 255, 0);
-  rect(this.x, this.y, this.size, this.size);
+  update() {
 
-}
-   
+    this.x = constrain(this.x, 0, width - this.size);
+    this.y = constrain(this.y, 0, height - this.size);
+  }
 
-move(keyCode) {
-  if (keyCode === LEFT_ARROW) this.x -= 20;
-  else if (keyCode === RIGHT_ARROW) this.x += 20;
-  else if (keyCode === UP_ARROW) this.y -= 20;
-  else if (keyCode === DOWN_ARROW) this.y += 20;
-}
+  show()  {
+
+    fill(0, 255, 0);
+    rect(this.x, this.y, this.size, this.size);
+
+  }
+
+  move(keyCode) {
+
+    if (keyCode === LEFT_ARROW) this.x -= 20;
+    else if (keyCode === RIGHT_ARROW) this.x += 20;
+    else if (keyCode === UP_ARROW) this.y -= 20;
+    else if (keyCode === DOWN_ARROW) this.y += 20;
+
+  }
+
 
 }
